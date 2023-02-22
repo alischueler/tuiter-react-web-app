768da51991b94e2b1952e4499b7a5ed25fc54750
Ali Schueler <ali.schueler456@gmail.com>
2023-02-21 20:15:53 -0500

    a6 spr2023

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -1,26 +1,27 @@
-import Nav from "../nav";
+import React from "react";
+import ExploreComponent from "./explore";
 import NavigationSidebar from "./navigation-sidebar";
-import WhoToFollowListItem from "./who-to-follow-list/who-to-follow-list-item";
-function Tuiter() {
-    return(
-        <div>
-            <Nav/>
-            <NavigationSidebar active="home"/>
-            <WhoToFollowListItem who={{
-                userName: 'NASA', handle: 'NASA',
-                avatarIcon: 'nasa.webp',
-            }}/>
-            <WhoToFollowListItem who={{
-                userName: 'Tesla', handle: 'tesla',
-                avatarIcon: 'tesla.jpeg',
-            }}/>
-            <WhoToFollowListItem who={{
-                userName: 'SpaceX', handle: 'SpaceX',
-                avatarIcon: 'spacex.svg',
-            }}/>
+import WhoToFollowList from "./who-to-follow-list";
+import Nav from "../nav";
 
-            <h1>Tuiter</h1>
+function Tuiter() {
+    return (
+        <div>
+        <Nav/>
+        <div className="row mt-2">
+            <div className="col-2 col-md-2 col-lg-1 col-xl-2">
+                <NavigationSidebar active="explore"/>
+            </div>
+            <div className="col-10 col-md-10 col-lg-7 col-xl-6"
+                 style={{"position": "relative"}}>
+                <ExploreComponent/>
+            </div>
+            <div className="d-none d-md-none d-lg-block col-lg-4 col-xl-4">
+                <WhoToFollowList/>
+            </div>
+        </div>
         </div>
     );
 }
+
 export default Tuiter
--- src/tuiter/who-to-follow-list/who-to-follow-list-item.js
+++ src/tuiter/who-to-follow-list/who-to-follow-list-item.js
@@ -1,22 +1,23 @@
 import React from "react";
+import "./who-to-follow-list-item.css"
 const WhoToFollowListItem = (
     {
-        who = { userName: 'NASA', handle: 'NASA', avatarIcon: 'tesla.jpeg' }
+        who = { userName: 'NASA', handle: 'NASA', avatarIcon: 'nasa.webp' }
     }
 ) => {
     return(
         <li className="list-group-item">
-            <div className="row">
-                <div className="col-2">
-                    <img className="rounded-circle" height={48} src={`../images/${who.avatarIcon}`}/>
+            <div className="d-flex justify-content-between">
+                <img src={`/images/${who.avatarIcon}`} height={48} width={48} className="rounded-circle align-self-center"
+                alt="who to follow icon"/>
+                <div className="ps-xxl-3 ps-xl-2 ps-l-1">
+                    <b>{who.userName}</b>
+                    <i className="fas fa-check-circle"/><br/>
+                    @{who.handle}
                 </div>
-                <div className="col-8">
-                    <div className="fw-bold">{who.userName}</div>
-                    <div>@{who.handle}</div>
-                </div>
-                <div className="col-2">
-                    <button className="btn btn-primary rounded-pill float-end">Follow</button>
-                </div>
+                <button type="button"
+                        className="btn btn-primary rounded-pill wd-height align-self-center ms-auto p-2">Follow
+                </button>
             </div>
         </li>
     );
